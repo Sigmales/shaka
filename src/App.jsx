4c073839f0f756c87a5916fd6b2a9766c911b98b
Sigmales <yantoubri@gmail.com>
2025-11-13 17:24:30 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,72 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import { Navbar } from './components/layout/Navbar'
-import { Footer } from './components/layout/Footer'
 import { ProtectedRoute } from './components/auth/ProtectedRoute'
 import { AdminRoute } from './components/auth/AdminRoute'
-
-// Pages
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Pricing from './pages/Pricing'
 import Dashboard from './pages/Dashboard'
+import Admin from './pages/Admin'
 import Predictions from './pages/Predictions'
+import Pricing from './pages/Pricing'
 import Payment from './pages/Payment'
 import Profile from './pages/Profile'
-import Admin from './pages/Admin'
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/pricing" element={<Pricing />} />
-              <Route
-                path="/dashboard"
-                element={(
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                )}
-              />
-              <Route
-                path="/predictions"
-                element={(
-                  <ProtectedRoute>
-                    <Predictions />
-                  </ProtectedRoute>
-                )}
-              />
-              <Route
-                path="/payment"
-                element={(
-                  <ProtectedRoute>
-                    <Payment />
-                  </ProtectedRoute>
-                )}
-              />
-              <Route
-                path="/profile"
-                element={(
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                )}
-              />
-              <Route
-                path="/admin"
-                element={(
-                  <AdminRoute>
-                    <Admin />
-                  </AdminRoute>
-                )}
-              />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/pricing" element={<Pricing />} />
+          
+          {/* Protected user routes */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Layout>
+                <Dashboard />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          <Route path="/predictions" element={
+            <ProtectedRoute>
+              <Layout>
+                <Predictions />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          <Route path="/payment" element={
+            <ProtectedRoute>
+              <Layout>
+                <Payment />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <Layout>
+                <Profile />
+              </Layout>
+            </ProtectedRoute>
+          } />
+          
+          {/* Admin routes */}
+          <Route path="/admin" element={
+            <AdminRoute>
+              <Layout>
+                <Admin />
+              </Layout>
+            </AdminRoute>
+          } />
+          
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </Router>
     </AuthProvider>
   )
